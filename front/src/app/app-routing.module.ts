730e98e47dfda15eff4583edb51507df1e723580
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2022-02-25 12:14:17 +0600

    Created basic app structure.

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtistsComponent } from './pages/artists/artists.component';
+import { AlbumsComponent } from './pages/albums/albums.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ArtistsComponent},
+  {path: 'artist/albums', component: AlbumsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ArtistsComponent } from './pages/artists/artists.component';
+import { AlbumsComponent } from './pages/albums/albums.component';
+import { ArtistComponent } from './ui/artist/artist.component';
+import { AlbumComponent } from './ui/album/album.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistsComponent,
+    AlbumsComponent,
+    ArtistComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +27,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     FlexLayoutModule,
     HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
